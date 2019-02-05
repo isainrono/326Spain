0842d2304fb7399d46a537dd755d889f210d0f5a
Isain Rodriguez Noreña <isainrodrigueznorena@iMac-de-Isain.local>
2019-02-05 20:24:06 +0100

    inicio 2  de pantalla search

---
+++ 326Spain/TableViewCellSearchCustom.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCellSearchCustom.swift
+//  326Spain
+//
+//  Created by Isain Rodriguez Noreña on 05/02/2019.
+//  Copyright © 2019 326Spain. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellSearchCustom: UITableViewCell {
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
